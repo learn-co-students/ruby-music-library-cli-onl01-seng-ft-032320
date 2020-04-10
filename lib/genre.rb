c0211aac72e28f1a55cc8ad37e11c72244de0412
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-04-10 02:44:35 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,22 @@
+class Genre
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+    @name = name
+    self.new(name).save
+  end
+end
