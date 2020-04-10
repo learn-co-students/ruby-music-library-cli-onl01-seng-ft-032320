f27d0cf16543737968f9e076f871efac9a769982
Samiylo Kryshu <skryshu@gmail.com>
2020-04-10 17:58:17 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,3 +1,33 @@
 class Genre 
   
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    
+    @@all << self 
+  end
+  
+  def save
+    @@all << self 
+  end
+  
+  # Class Methods
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def self.create(name)
+    genre = self.new(name)
+    genre.save
+    genre
+    
+  end
 end
